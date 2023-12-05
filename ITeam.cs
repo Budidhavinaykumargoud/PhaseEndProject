2cab8f6be128f20f84c8ae628e430f0e7a3e5c8c
Budidhavinaykumargoud <vinaygoud911@gmail.com>
2023-12-05 21:05:03 +0530

    TeamManagementApp

---
+++ ITeam.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace TeamManagementApp
+{
+    public interface ITeam
+    {
+        void Add(Player player);
+        void Remove(int playerId);
+        Player GetPlayerById(int playerId);
+        Player GetPlayerByName(string name);
+        List<Player> GetAllPlayers();
+    }
+}
